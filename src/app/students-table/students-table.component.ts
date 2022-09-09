c32a1054d242d15a00826a62dc546dfb96695583
ASB24 <alexserafini2001@gmail.com>
2022-09-09 17:58:41 -0400

    create app-student-table component
    With pagination and sorting, will add filtering and action column for easier CRUD operation

---
+++ src/app/students-table/students-table.component.ts
@@ -0,0 +1,38 @@
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { Student } from 'src/interfaces';
+import { SchoolService } from '../school.service';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-students-table',
+  templateUrl: './students-table.component.html',
+  styleUrls: ['./students-table.component.sass']
+})
+export class StudentsTableComponent implements AfterViewInit {
+
+  dataSource : MatTableDataSource<Student> = new MatTableDataSource()
+  tableColumns : string[] = ['id','name','lastName','class']
+  
+  constructor(private SchoolService: SchoolService, private changeDetector : ChangeDetectorRef) { 
+    this.SchoolService.getStudents().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data)
+    })
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  @ViewChild(MatSort) sort!: MatSort
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+
+  ngAfterViewInit() {
+    setTimeout(()=>{
+      this.dataSource.sort = this.sort
+      this.dataSource.paginator = this.paginator
+    })
+  }
+
+}

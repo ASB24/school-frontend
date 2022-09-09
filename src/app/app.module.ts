7e37c269ded39a5d0e4924365157734d7450d746
ASB24 <alexserafini2001@gmail.com>
2022-09-09 18:00:01 -0400

    refactor app html
    removed stock generated main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './material/material.module';
-import { StudentsComponent } from './students/students.component'
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentsTableComponent } from './students-table/students-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent
+    StudentFormComponent,
+    StudentsTableComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule
   ],

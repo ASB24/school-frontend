34ab42203e8de54e2e955942754641b0c14dfb64
ASB24 <alexserafini2001@gmail.com>
2022-09-12 02:45:05 -0400

    trial to add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { AttendanceListComponent } from './attendance-list/attendance-list.component'
+import { GradeComponentComponent } from './grade-component/grade-component.component'
+import { StudentPackageComponent } from './student-package/student-package.component'
 
 const routes: Routes = [
-  
+  {path: 'students', component: StudentPackageComponent},
+  {path: 'grades', component: GradeComponentComponent},
+  {path: 'attendances', component: AttendanceListComponent}
 ]
 
 @NgModule({
